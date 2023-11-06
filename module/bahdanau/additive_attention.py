abd10e4ed4fc829046ec3fdb501c20c32cac9c79
Shakleen Ishfar <shakleenishfar@gmail.com>
2023-11-06 10:33:22 -0500

    Implement additive attention

---
+++ module/bahdanau/additive_attention.py
@@ -0,0 +1,31 @@
+import torch
+from torch import nn
+
+from .masked_softmax import masked_softmax
+
+
+class AdditiveAttention(nn.Module):
+    """Additive attention."""
+
+    def __init__(self, num_hiddens, dropout, **kwargs):
+        super(AdditiveAttention, self).__init__(**kwargs)
+        self.W_k = nn.LazyLinear(num_hiddens, bias=False)
+        self.W_q = nn.LazyLinear(num_hiddens, bias=False)
+        self.w_v = nn.LazyLinear(1, bias=False)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, queries, keys, values, valid_lens):
+        queries, keys = self.W_q(queries), self.W_k(keys)
+        # After dimension expansion,
+        # shape of queries: (batch_size, no. of # queries, 1, num_hiddens) and
+        # shape of keys: (batch_size, 1, no. of # key-value pairs, num_hiddens).
+        # Sum them up with broadcasting
+        features = queries.unsqueeze(2) + keys.unsqueeze(1)
+        features = torch.tanh(features)
+        # There is only one output of self.w_v, so we remove the last
+        # one-dimensional entry from the shape. Shape of scores: (batch_size,
+        # no. of queries, no. of key-value pairs)
+        scores = self.w_v(features).squeeze(-1)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        # Shape of values: (batch_size, no. of key-value pairs, value dimension)
+        return torch.bmm(self.dropout(self.attention_weights), values)
